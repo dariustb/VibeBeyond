81a96acee32e8468df2e78b864e7d4555976acd3
Darius Brown <dariustb@tech-center.com>
2023-01-18 22:32:36 +0000

    Creating start screen (#23)
    * Removed boilerplate images
    
    * Moved imperf public/ -> assets/
    
    * Changed vue title 'Tauri + Vue 3 App' -> 'Vibe Beyond'
    
    * Added Montserrat font
    
    * Added light grey translucent bkgd filter
    
    * Added title/play button/credits/semi overlay

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,23 @@
 </script>
 
 <template>
+    <!-- Main container -->
+    <div class="flex items-center justify-center h-screen ">
+        <!-- vibe beyond title -->
+        <h1 class="text-[64px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+            <b class="font-light">vibe </b><b>beyond.</b>
+        </h1>
+
+        <!-- play button + text -->
+        <div class="absolute bottom-52 drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]" id="play-button">
+            <p class="text-2xl text-[#4F3247] opacity-80 tracking-wider text-center relative bottom-4">play</p>            
+        </div>
+
+        <!-- Darius / Linh -->
+        <div class ="absolute right-5 bottom-3">
+            <p class="text-xs opacity-50 font-semibold">Darius Brown / Linh Nguyen</p>
+        </div>
+    </div>
 </template>
 
 <style scoped>
