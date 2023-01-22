7d7b22d37c3c64e2a1395a379de73fd67278a85c
Darius Brown <dariustb@tech-center.com>
2023-01-22 00:56:58 -0500

    Small changes

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ export default {
     <div v-show="startPage" class="flex items-center justify-center h-screen shadow-[inset_0_0px_0px_1000px_rgba(217,217,217,0.2)]">
         <!-- vibe beyond title -->
         <div class="fade-in">
-            <h1 class="text-[64px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] typewriter">
+            <h1 class="text-[64px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
                 <b class="font-light">vibe </b><b>beyond.</b>
             </h1>
         </div>
