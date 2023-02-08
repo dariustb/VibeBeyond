bf25936caed44cb1efd62c9cdba62eca851b9786
Darius Brown <dariustb@tech-center.com>
2023-02-08 06:57:39 -0500

    Added start page

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,24 @@
 </script>
 
 <template>
+  <!-- Start Page -->
+  <div class="flex items-center justify-center h-screen shadow-[inset_0_0px_0px_1000px_rgba(217,217,217,0.2)]">
+    <!-- vibe beyond title -->
+    <h1 class="text-[64px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+        <b class="font-light">vibe </b><b>beyond.</b>
+    </h1>
+
+    <!-- play button + text -->
+    <button class="absolute bottom-52 drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]" id="play-button">
+        <p class="text-2xl text-[#4F3247] opacity-80 tracking-wider text-center relative bottom-4">play</p>            
+    </button>
+
+    <!-- Darius / Linh -->
+    <div class ="absolute right-5 bottom-3">
+        <p class="text-xs opacity-50 font-semibold">Darius Brown / Linh Nguyen</p>
+    </div>
+  </div>
+  
 </template>
 
 <style scoped>
