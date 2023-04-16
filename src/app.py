afe729d809da4b74ce4d0c7da73d067306a67588
Darius Brown <dariustb@tech-center.com>
2023-04-16 15:25:08 -0400

    Moved testing if __main__ to driver

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 ''' app.py - Python driver for Flask server '''
 
 from flask import Flask, render_template
+from py import song
+from py import soundfont as sf2
 
 app = Flask(__name__)
 
@@ -20,7 +22,23 @@ def play():
         song_name = song_name,
         song_artist = song_artist,
         song_length = song_length
-        )
+    )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+
+    # Create song
+    song = song.Song()
+
+    # Create midi file of the song
+    song.gen_chord_prog()
+    MIDI_PATH = song.save_midi_file()
+
+    song.print_info()
+
+    # Convert midi file to audio
+    sf2.midi_to_audio(
+        midi_path   = MIDI_PATH,
+        output_path = 'src/gen/audio/output.wav',
+        sf2_path    = 'src/static/sf2/rhodes.sf2',
+    )
