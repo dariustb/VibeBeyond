82eeca184139e3c1909300ba40f80efbc66a7bfa
Darius Brown <dariustb@tech-center.com>
2023-04-18 20:16:21 -0400

    Added os.remove() for temp file cleanup

--- tests/py/test_audio_manip.py
+++ tests/py/test_audio_manip.py
@@ -1,5 +1,6 @@
 ''' test_audio_manip.py - Unit tests for audio_manip.py '''
 
+import os
 from src.py import audio_manip # pylint: disable = import-error
 
 TEST_FILE_PATH = 'tests/test_files/'
@@ -8,18 +9,24 @@ TEST_CHORDS = TEST_FILE_PATH + 'test_chord_prog.wav'
 
 def test_combine_audios():
     ''' test_combine_audios - Tests the combine_audios function '''
-    assert audio_manip.combine_audios(
-        audio_paths=(
-            TEST_CHORDS,
-            TEST_DRUMS,
-        ),
-        output_path= TEST_FILE_PATH + 'test_result_combine_audio.wav'
-    )
+    try:
+        assert audio_manip.combine_audios(
+            audio_paths=(
+                TEST_CHORDS,
+                TEST_DRUMS,
+            ),
+            output_path= TEST_FILE_PATH + 'test_result_combine_audio.wav'
+        )
+    finally:
+        os.remove(TEST_FILE_PATH + 'test_result_combine_audio.wav')
 
 def test_loop_audio():
     ''' test_loop_audio - Tests the loop_audio function '''
-    assert audio_manip.loop_audio(
-        audio_path  = TEST_DRUMS,
-        output_path = TEST_FILE_PATH + 'test_result_loop_audio.wav',
-        loop_count  = 4
-    )
+    try:
+        assert audio_manip.loop_audio(
+            audio_path  = TEST_DRUMS,
+            output_path = TEST_FILE_PATH + 'test_result_loop_audio.wav',
+            loop_count  = 4
+        )
+    finally:
+        os.remove(TEST_FILE_PATH + 'test_result_loop_audio.wav')
