c9d4e36cce5e3398854ad049409b1ad70863daa6
Darius Brown <dariustb@tech-center.com>
2023-04-18 20:10:19 -0400

    Added audio_manip tests

---
+++ tests/py/test_audio_manip.py
@@ -0,0 +1,25 @@
+''' test_audio_manip.py - Unit tests for audio_manip.py '''
+
+from src.py import audio_manip # pylint: disable = import-error
+
+TEST_FILE_PATH = 'tests/test_files/'
+TEST_DRUMS = TEST_FILE_PATH + 'test_drums.wav'
+TEST_CHORDS = TEST_FILE_PATH + 'test_chord_prog.wav'
+
+def test_combine_audios():
+    ''' test_combine_audios - Tests the combine_audios function '''
+    assert audio_manip.combine_audios(
+        audio_paths=(
+            TEST_CHORDS,
+            TEST_DRUMS,
+        ),
+        output_path= TEST_FILE_PATH + 'test_result_combine_audio.wav'
+    )
+
+def test_loop_audio():
+    ''' test_loop_audio - Tests the loop_audio function '''
+    assert audio_manip.loop_audio(
+        audio_path  = TEST_DRUMS,
+        output_path = TEST_FILE_PATH + 'test_result_loop_audio.wav',
+        loop_count  = 4
+    )
