5af2fb367fec5f366f8c77778079d63f57339df7
Darius Brown <dariustb@tech-center.com>
2023-04-18 22:13:25 -0400

    Added timeout argument to prevent hanging

--- tests/flask/test_flask.py
+++ tests/flask/test_flask.py
@@ -5,7 +5,7 @@ import requests
 def test_server_not_running():
     ''' Tests if the server is not running.'''
     try:
-        response = requests.get("http://localhost:5000/")
+        response = requests.get("http://localhost:5000/", timeout=(5, 5))
         assert response.status_code != 200
     except requests.exceptions.ConnectionError:
         assert True
