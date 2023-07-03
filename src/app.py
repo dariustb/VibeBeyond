22cb4de35054c87185979c004094f2ae4b275c78
Darius Brown <dariustb@tech-center.com>
2023-07-02 23:35:10 -0400

    Created application window

--- src/app.py
+++ src/app.py
@@ -1,1 +1,20 @@
+''' app.py - driver program for the application '''
+
+# pylint: disable = invalid-name, no-member
+
 import pygame
+
+pygame.init()
+
+SCREEN_SIZE: tuple = 800, 600 # width, height
+
+if __name__ == '__main__':
+
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+
+    is_window_open = True
+
+    while is_window_open:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_window_open = False
