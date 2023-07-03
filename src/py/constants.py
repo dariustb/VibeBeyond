36bc01d3f3830f5046b50b686c6fbb0fe7e8c54e
Darius Brown <dariustb@tech-center.com>
2023-07-03 02:14:30 -0400

    Refactored files

--- src/py/constants.py
+++ src/py/constants.py
@@ -3,9 +3,13 @@
 # App metadata
 APP_NAME: str = 'Vibe Beyond'
 APP_VERSION: str = '2.0.0'
+APP_ICON: str = 'src/assets/images/icon256x256.png'
 
 # Pygame window
 SCREEN_WIDTH: int = 800
 SCREEN_HEIGHT: int = 600
 SCREEN_SIZE: tuple = SCREEN_WIDTH, SCREEN_HEIGHT
 SCREEN_BKGD: tuple = 181, 126, 220
+
+# Pygame mixer
+DEFAULT_VOLUME: float = 0.7
