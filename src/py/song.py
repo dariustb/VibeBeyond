4426ead189d6fbe0aad1aa7d04cc0ae356040d88
Darius Brown <dariustb@tech-center.com>
2023-07-09 09:31:25 -0400

    Refactored utility functions

--- src/py/song.py
+++ src/py/song.py
@@ -5,11 +5,11 @@
 import random
 import string
 import os
-from re import sub
 
 import mido
 from pydub import AudioSegment
 from py.constants import *
+import py.song_utils as util
 
 class Song:
     ''' This class will be used to generate a song'''
@@ -120,72 +120,12 @@ class Song:
         return track
 
     # GENERATION FUNCTIONS
-    def get_chord_intervals_list(self) -> list:
-        ''' Returns a list of chord intervals in the progression '''
-        chord_intervals_list = []
-
-        for chord in self.prog:
-            # Evaluate chord type as intervals
-            chord_no_acc = chord.replace('b','').replace('#','')
-            if '7' in chord:
-                chord_intervals = (0, 4, 7, 10)  # dominant 7th
-            elif 'dim' in chord:
-                chord_intervals = (0, 3, 6, 9)   # diminished
-            elif 'aug' in chord:
-                chord_intervals = (0, 4, 8, 10)  # augmented
-            elif chord.lower() == chord:
-                chord_intervals = (0, 3, 7, 10)  # minor 7
-            elif chord_no_acc.upper() == chord_no_acc:
-                chord_intervals = (0, 4, 7, 11)  # major 7
-            else:
-                raise ValueError('Invalid chord type')
-
-            chord_intervals_list.append(chord_intervals)
-
-        return chord_intervals_list
-
-    def get_root_note_list(self) -> list:
-        ''' Returns a list of root notes in the progression '''
-        root_note_list = []
-
-        for chord in self.prog:
-            # Evaluate key value as root note
-            root_note = {
-                'Gb': 54, 'G': 55,
-                'Ab': 56, 'A': 57,
-                'Bb': 58, 'B': 59,
-                'C': 60,
-                'Db': 61, 'D': 62,
-                'Eb': 63, 'E': 64,
-                'F': 65
-            }[self.key]
-
-            # Evaluate scale degree + add interval to root note
-            chord_degree = sub(r'b|#|7|dim|aug|m|M', '', chord).lower()
-            root_note += {
-                'i':   0, 'ii': 2,
-                'iii': 4, 
-                'iv': -7, 'v':  -5,
-                'vi': -3, 'vii':-1
-            }[chord_degree]
-
-            # Evaluate scale degree modification (flat, sharp, etc.)
-            if 'b' in chord:
-                root_note -= 1   # flat/lowered half-step
-            elif '#' in chord:
-                root_note += 1   # sharp/raised half-step
-
-            # Add values to outside lists
-            root_note_list.append(root_note)
-
-        return root_note_list
-
     def gen_chord_prog(self) -> bool:
         ''' Adds a chord progression to the class variable '''
 
         # Get chord progression variables
-        chord_intervals_list    = self.get_chord_intervals_list()
-        root_note_list          = self.get_root_note_list()
+        chord_intervals_list    = util.get_chord_intervals_list(self.prog)
+        root_note_list          = util.get_root_note_list(self.key, self.prog)
 
         # Repeat chord progression for length of song
         for _ in range(SONG_LENGTH):
@@ -245,9 +185,9 @@ class Song:
         hat_segment = []
 
         for _ in range(len(self.prog)):
-            coordinate_sample(kick_audio, kick_segment, kick_pattern, bpm_in_ms)
-            coordinate_sample(hat_audio, hat_segment, hat_pattern, bpm_in_ms)
-            coordinate_snare(snare_audio, snare_segment, bpm_in_ms) # Snare is hardcoded 2 & 4
+            util.coordinate_sample(kick_audio, kick_segment, kick_pattern, bpm_in_ms)
+            util.coordinate_sample(hat_audio, hat_segment, hat_pattern, bpm_in_ms)
+            util.coordinate_snare(snare_audio, snare_segment, bpm_in_ms)
 
         # Combine sample and silences (per instrument)
         kick_segment = sum(kick_segment)
@@ -293,29 +233,3 @@ class Song:
             final_audio = final_audio.overlay(self.drum_segment,0)
 
         final_audio.export(self.song_path, format=AUDIO_FILE_TYPE)
-
-def coordinate_sample(audio, segment, pattern, bpm_in_ms):
-    ''' coordinates sample audio to the rhythmic pattern passed in'''
-    for _ in range(SONG_LENGTH):
-        for note in pattern:
-            note_length_in_ms = ((note + 1)/BASE_NOTE) * bpm_in_ms
-            if len(audio) < note_length_in_ms:
-                segment.append(audio)
-                segment.append(AudioSegment.silent(note_length_in_ms - len(audio)))
-            elif len(audio) > note_length_in_ms:
-                shortened_audio = audio[:len(audio) - note_length_in_ms]
-                segment.append(shortened_audio)
-
-def coordinate_snare(audio, segment, bpm_in_ms):
-    ''' coordinates snare to the 2 and 4 of the beat '''
-    note_length_in_ms = ((HALF_NOTE + 1)/BASE_NOTE) * bpm_in_ms
-    segment.append(AudioSegment.silent(note_length_in_ms/2))
-    for _ in range(SONG_LENGTH - 1):
-        segment.append(audio)
-        segment.append(AudioSegment.silent(note_length_in_ms - len(audio)))
-        segment.append(audio)
-        segment.append(AudioSegment.silent(note_length_in_ms - len(audio)))
-    segment.append(audio)
-    segment.append(AudioSegment.silent(note_length_in_ms - len(audio)))
-    segment.append(audio)
-    segment.append(AudioSegment.silent(note_length_in_ms/2))
