279e0f0c397e6ed5ac48ca203f00e6615ea26603
Darius Brown <dariustb@tech-center.com>
2023-08-30 22:18:02 -0400

    Refactored sanity test

---
+++ tests/test_sanity.py
@@ -0,0 +1,7 @@
+''' test_sanity.py - Unit tests to make sure pytest is running correctly '''
+
+def test_sanity():
+    ''' test_sanity - test that pytest is running'''
+
+    # Given/When/Then
+    assert True
