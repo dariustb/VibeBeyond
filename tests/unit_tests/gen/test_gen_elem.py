826e62eed4a0242d7e44970146644bb18f14aae2
Darius Brown <dariustb@tech-center.com>
2023-08-30 22:28:00 -0400

    Pylint fixes

--- tests/unit_tests/gen/test_gen_elem.py
+++ tests/unit_tests/gen/test_gen_elem.py
@@ -1,13 +1,17 @@
 ''' test_gen_elem.py - unit test for gen_elem.py '''
 
+# pylint: disable = W0621, E0401
+
 import pytest
 from src.py.gen.gen_elem import SongElements
 
 @pytest.fixture
 def elem():
+    '''SongElement fixture'''
     return SongElements()
 
 def test_attributes(elem):
+    '''Test SongElement attribute types'''
     assert isinstance(elem.key,  str)
     assert isinstance(elem.time, tuple)
     assert isinstance(elem.bpm,  int)
