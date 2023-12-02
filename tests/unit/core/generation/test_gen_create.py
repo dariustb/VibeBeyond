8d47d767cb68885c3a0b4a7ec6634039644c129b
Darius Brown <dariustb@tech-center.com>
2023-12-01 22:53:41 -0500

    Format changes

--- tests/unit/core/generation/test_gen_create.py
+++ tests/unit/core/generation/test_gen_create.py
@@ -1,10 +1,12 @@
 """ test_gen_create - Unit tests for gen_create """
 
+# pylint: disable = E0401
+
 from src.core.generation.gen_create import create_song
 
 
 def test_create_song():
-    ''' test create_song() '''
+    """test create_song()"""
     # Given
     # When
     test_song = create_song()
