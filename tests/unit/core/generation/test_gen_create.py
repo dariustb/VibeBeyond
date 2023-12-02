4f7f50aba9b8e3dfeb9f87296e92491bea81f5c2
Darius Brown <dariustb@tech-center.com>
2023-12-01 22:49:20 -0500

    Added core

---
+++ tests/unit/core/generation/test_gen_create.py
@@ -0,0 +1,13 @@
+""" test_gen_create - Unit tests for gen_create """
+
+from src.core.generation.gen_create import create_song
+
+
+def test_create_song():
+    ''' test create_song() '''
+    # Given
+    # When
+    test_song = create_song()
+
+    # Then
+    assert isinstance(test_song, str)
