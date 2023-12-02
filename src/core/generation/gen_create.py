65994a21bf42b1e74394a82c1c0dfa89023fc03a
Darius Brown <dariustb@tech-center.com>
2023-12-02 14:04:26 -0500

    Created checklist for create_song()

--- src/core/generation/gen_create.py
+++ src/core/generation/gen_create.py
@@ -1,6 +1,20 @@
 """ gen_create - houses the create_song function """
 
 
+# The feast de resistance
 def create_song() -> str:
     """create_song - builds song and returns the song file path"""
+
+    # 1. RNG Song Elements
+
+    # 2. Generate MIDI Loops
+
+    # 3. Generate Audio Loops
+
+    # 4. Generate Song Segments
+
+    # 5. Combine Segments into Final Audio
+
+    # 6. Clean up
+
     return "src/assets/vb_example_song.wav"
