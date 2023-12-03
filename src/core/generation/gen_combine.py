cdf7b54bb037290c7c6d805b90b45169502825fb
Darius Brown <dariustb@tech-center.com>
2023-12-03 15:19:18 -0500

    Added combining functionality

--- src/core/generation/gen_combine.py
+++ src/core/generation/gen_combine.py
@@ -5,8 +5,6 @@ from pydub import AudioSegment
 from .gen_segment import SongSegmentGen
 
 # Constant variables
-AUDIO_FOLDER: str = "src/gen/audio"
-AUDIO_FILE_TYPE: str = ".wav"
 AUDIO_TYPE: str = "wav"
 
 
@@ -19,22 +17,15 @@ class SongCombine:
         # Audio segment
         self.song_segment: AudioSegment = None
 
-        # Audio file paths
-        self.song_path: str = AUDIO_FOLDER + "song" + AUDIO_FILE_TYPE
-
     def combine_segments(self, Segments: SongSegmentGen) -> AudioSegment:
         """Combines segments into one whole segment"""
-        song_duration = len(Segments.drum_segment)
+        song_duration = len(Segments.chords_segment)
         self.song_segment = AudioSegment.silent(song_duration)
 
-        if Segments.ambient_segment is not None:
-            self.song_segment = self.song_segment.overlay(Segments.ambient_segment, 0)
-        if Segments.melody_segment is not None:
-            self.song_segment = self.song_segment.overlay(Segments.melody_segment, 0)
-        if Segments.cmelody_segment is not None:
-            self.song_segment = self.song_segment.overlay(Segments.cmelody_segment, 0)
         if Segments.chords_segment is not None:
             self.song_segment = self.song_segment.overlay(Segments.chords_segment, 0)
+        if Segments.melody_segment is not None:
+            self.song_segment = self.song_segment.overlay(Segments.melody_segment, 0)
         if Segments.drum_segment is not None:
             self.song_segment = self.song_segment.overlay(Segments.drum_segment, 0)
 
@@ -43,5 +34,6 @@ class SongCombine:
     # EXPORT FUNCTIONS
     def export_audio_from_segment(self) -> str:
         """creates an audio file with all the AudioSegments combined"""
-        self.song_segment.export(self.song_path, format=AUDIO_TYPE)
-        return self.song_path
+        song_path = "src/gen/audio/song.wav"
+        self.song_segment.export(song_path, format=AUDIO_TYPE)
+        return song_path
