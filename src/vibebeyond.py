39121068c26317eca676a31c28c20bcd45036de9
Darius Brown <dariustb@tech-center.com>
2023-12-03 21:42:20 -0500

    Added images for UI

--- src/vibebeyond.py
+++ src/vibebeyond.py
@@ -15,6 +15,16 @@ DEFAULT_SCREEN_SIZE: tuple = (DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT)
 DEFAULT_SCREEN_BKGD: tuple = (242, 218, 201)  # Champagne Pink
 DEFAULT_MIXER_VOLUME: float = 0.7
 
+PLAY_BUTTON_LOCATION: tuple = (65, 44)  # X-Y coords from Figma
+PUASE_BUTTON_LOCATION: tuple = (60, 45)
+VOLUME_SLIDER_LOCATION: tuple = (
+    (213, 71),
+    (245, 71),
+    (277, 71),
+    (309, 71),
+    (341, 71),
+)
+
 EVENT_SONG_ENDED: int = pygame.USEREVENT + 616  # pylint: disable = E1101
 
 if __name__ == "__main__":
@@ -27,6 +37,28 @@ if __name__ == "__main__":
     pygame.display.set_caption(APP_NAME)
     pygame.display.set_icon(pygame.image.load(APP_ICON))
     pygame.display.update()
+
+    # Set up app screen
+    skin_screen = pygame.image.load("src/assets/_media/vb_skin_400x150.png")
+    play_button = pygame.image.load("src/assets/_media/vb_play_button_54x62.png")
+    pause_button = pygame.image.load("src/assets/_media/vb_pause_button_48x61.png")
+    inactive_vol = pygame.image.load(
+        "src/assets/_media/vb_volume_button_inactive_10x10.png"
+    )
+    active_vol = pygame.image.load(
+        "src/assets/_media/vb_volume_button_active_10x10.png"
+    )
+
+    volume_index = 2
+    scrn.blit(skin_screen, (0, 0))
+    scrn.blit(play_button, PLAY_BUTTON_LOCATION)
+    for index, location in enumerate(VOLUME_SLIDER_LOCATION):
+        if index == volume_index:
+            scrn.blit(active_vol, location)
+        else:
+            scrn.blit(inactive_vol, location)
+
+    pygame.display.flip()
 
     # Set up music player
     pygame.mixer.init()
