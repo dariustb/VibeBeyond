0a9241a0607cde51918437a04a1a1d63ae9dec1f
Darius Brown <dariustb@tech-center.com>
2024-02-19 06:58:30 -0500

    Added outline

--- src/groovegen.m.cpp
+++ src/groovegen.m.cpp
@@ -4,8 +4,21 @@
 using namespace std;
 
 int main() {
-    // Set timer for rand()
+    // 1. RNG Song Elements
     srand(time(NULL));
+    Elements SongElements;
+
+    // 2. Generate MIDI Loops
+
+    // 3. Generate Audio Loops
+
+    // 4. Generate Drum Loops
+
+    // 5. Generate Song Segments
+
+    // 6. Combine Segments into Final Audio
+
+    // 7. Clean up
 
     return 0;
 }
