ab6f3e44590153b6aed0670ff9481e209d8b825e
Darius Brown <dariustb@tech-center.com>
2024-03-02 06:00:00 -0500

    Removed unused comments

--- src/groovegen.m.cpp
+++ src/groovegen.m.cpp
@@ -1,24 +1,9 @@
-#include <groovegen_elements.h>
 #include <iostream>
+#include <RtMidi.h>
 
 using namespace std;
 
 int main() {
-    // 1. RNG Song Elements
-    // srand(time(NULL));
-    // Elements SongElements;
-
-    // 2. Generate MIDI Loops
-
-    // 3. Generate Audio Loops
-
-    // 4. Generate Drum Loops
-
-    // 5. Generate Song Segments
-
-    // 6. Combine Segments into Final Audio
-
-    // 7. Clean up
 
     cout << "Hello world!" << endl;
 
